6d0d7f78840ee9d62c4ef82bdc3a2251549ac1f4
Naufal-Nalendra <16521240@std.stei.itb.ac.id>
2023-04-07 15:52:45 +0700

    feat : input desired nodes

--- src/input.py
+++ src/input.py
@@ -11,14 +11,25 @@ def inputFile():
         # Get the number of nodes
         size = len(name)
         # Check if the number of nodes is at least 8
-        if size >= 8:
-            print(" ".join(name))
+        if size >= 0:
             # Reading the adjacency matrix
             matrix = []
             for i in range(size):
                 line = f.readline()
                 row = [int(x) for x in line.strip().split()]
+                rowLength = len(row)
+                if(rowLength != size):
+                    print("Matriks ketetanggaan tidak valid")
+                    inputFile()
+                    break
+                for rows in row:
+                    if(rows < 0):
+                        print("Elemen matriks ketetanggaan tidak valid")
+                        inputFile()
+                        break
                 matrix.append(row)
+            print("\nDaftar simpul:")
+            print(" ".join(name))
             # Print the adjacency matrix
             print("\nMatriks ketetanggaan:")
             for row in matrix:
@@ -30,9 +41,34 @@ def inputFile():
             print("Silahkan masukkan file dengan setidaknya 8 simpul")
             # Call the function again
             inputFile()
-inputFile()
 
-
+def inputRequest(name : list):
+    print("\nDaftar simpul valid:")
+    for i in range(len(name)):
+        print(f"{i+1}. {name[i]}")
+    inputNode = int(input("Masukkan simpul awal: "))
+    while (inputNode < 1) or (inputNode > len(name)):
+        print("Masukkan simpul awal yang valid")
+        inputNode = int(input("Masukkan simpul awal: "))
+    startNode = inputNode-1
     
+    print("\nDaftar simpul valid:")
+    for i in range(len(name)):
+        if (i < startNode):
+            print(f"{i+1}. {name[i]}")
+        elif (i > startNode):
+            print(f"{i}. {name[i]}")
+    inputNode = int(input("Masukkan simpul tujuan: "))
+    while (inputNode < 1) or (inputNode > len(name)-1):
+        print("Masukkan simpul tujuan yang valid")
+        inputNode = int(input("Masukkan simpul tujuan: "))
+    if (inputNode-1 < startNode):
+        endNode = inputNode-1
+    else:
+        endNode = inputNode
+    return startNode, endNode
 
-    
+name, matrix = inputFile()
+startNode, endNode = inputRequest(name)
+print(f"\nSimpul awal: {name[startNode]}")
+print(f"Simpul tujuan: {name[endNode]}")
